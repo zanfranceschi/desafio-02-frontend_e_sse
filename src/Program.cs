5f69ebb2398de523a26a751131290c48107e4f7d
Leonardo-sm <leonardo.smichelutti@gmail.com>
2022-07-24 15:41:33 -0300

    fix: adicionado politica de cors

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,5 @@
+var CorsPolicy = "_myCorsPolicy";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy(CorsPolicy, builder => builder.AllowAnyOrigin());
+});
 
 var app = builder.Build();
 
@@ -15,6 +21,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(CorsPolicy);
 
 app.UseHttpsRedirection();
 
